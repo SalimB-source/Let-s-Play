f5c1b1927a38569b81691e53804e68178f5998d1
SalimB-source <2.84957636e+08+SalimB-source@users.noreply.github.com>
2026-09-17 10:36:57 +0000

    Fix Vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Let-s-Play/',
+  base: process.env.VERCEL ? '/' : '/Let-s-Play/',
   server: {
     allowedHosts: true,
   },
